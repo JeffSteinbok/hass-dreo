b6084600549e22eda3337791bc7ebffe2c53bf3d
Jeff Steinbok <jeff@steinbok.net>
2024-05-07 22:46:16 -0700

    Revert "Apply fix for HA 2024-05 (#91)"
    This reverts commit 4f3a5f2bea139131e1f1788570f6723f7e1da785.

--- custom_components/dreo/basedevice.py
+++ custom_components/dreo/basedevice.py
@@ -44,11 +44,12 @@ class DreoBaseDeviceHA(Entity):
     def should_poll(self):
         return False
 
-    def async_added_to_hass(self):
+    async def async_added_to_hass(self):
         """Register callbacks."""
 
+        @callback
         def update_state():
             # Tell HA we're ready to update
-            self.schedule_update_ha_state(True)
+            self.async_schedule_update_ha_state(True)
 
         self.device.add_attr_callback(update_state)
