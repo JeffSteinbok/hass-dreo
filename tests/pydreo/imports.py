a5b7ad571e4fc367dcb866afc8934bc9d0c8ab9e
Jeff Steinbok <jeff@steinbok.net>
2024-08-24 15:57:50 -0700

    Update test includes

--- tests/pydreo/imports.py
+++ tests/pydreo/imports.py
@@ -4,10 +4,10 @@ import importlib.util
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from custom_components.dreo.pydreo import PyDreo, PyDreoBaseDevice
+    from custom_components.dreo.pydreo import PyDreo, PyDreoBaseDevice, Helpers
 else:
     spec = importlib.util.spec_from_file_location("pydreo", "custom_components/dreo/pydreo/__init__.py")
     pydreo = importlib.util.module_from_spec(spec)
     sys.modules["pydreo"] = pydreo
     spec.loader.exec_module(pydreo)
-    from pydreo import PyDreo, PyDreoBaseDevice
+    from pydreo import PyDreo, PyDreoBaseDevice, Helpers
