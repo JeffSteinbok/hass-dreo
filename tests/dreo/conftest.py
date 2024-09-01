a2b113a862abfbb0327e3c0055462a4c5275cf66
Jeff Steinbok <jeff@steinbok.net>
2024-09-01 00:38:35 -0700

    First working HA UT

--- tests/dreo/conftest.py
+++ tests/dreo/conftest.py
@@ -5,24 +5,6 @@ import pytest
 from custom_components.dreo.pydreo import PyDreo
 
 
-@pytest.fixture
-def mocked_pydreo(mocker, mocked_dreo):
-    #mocker.patch("homeassistant.helpers.entity.Entity.async_write_ha_state")
-    pydreo_mock = mocker.patch.object(
-        PyDreo, "HubSpaceDataUpdateCoordinator", autospec=True
-    )
-    pydreo_mock.conn = mocked_dreo
-    pydreo_mock.data = defaultdict(dict)
-    yield pydreo_mock
-
-
-@pytest.fixture
-def mocked_pydreo(mocker):
-    """Mock all HubSpace functionality but ensure the class is correct"""
-    hs_mock = mocker.patch.object(pydreo_async, "HubSpaceConnection", autospec=True)
-    yield hs_mock
-
-
 @pytest.fixture(autouse=True)
 def set_debug_mode(caplog):
     # Force capture of all debug logging. This is useful if you want to verify
