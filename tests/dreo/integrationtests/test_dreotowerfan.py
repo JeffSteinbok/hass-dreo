1f3088d4a3abfd5b0cfd7cda15f4c746a2eff99d
Jeff Steinbok <jeff@steinbok.net>
2024-09-06 12:15:37 -0700

    UT Fix

--- tests/dreo/integrationtests/test_dreotowerfan.py
+++ tests/dreo/integrationtests/test_dreotowerfan.py
@@ -15,33 +15,6 @@ LOGIN_RESPONSE = call_json.LOGIN_RET_BODY
 
 class TestPyDreoFan(IntegrationTestBase):
     """Test PyDreoFan class."""
-    def test_tower_load_and_send_commands(self):
-        """Load fan and test sending commands."""
-
-        self.get_devices_file_name = "get_devices_HTF008S.json"
-        self.pydreo_manager.load_devices()
-        assert len(self.pydreo_manager.devices) == 1
-        fan = self.pydreo_manager.devices[0]
-        assert fan.speed_range == (1, 5)
-        assert fan.preset_modes == ['normal', 'natural', 'sleep', 'auto']
-
-        with patch(PATCH_SEND_COMMAND) as mock_send_command:
-            fan.is_on = True
-            mock_send_command.assert_called_once_with(fan, {POWERON_KEY: True})
-
-        with patch(PATCH_SEND_COMMAND) as mock_send_command:
-            fan.preset_mode = 'normal'
-            mock_send_command.assert_called_once_with(fan, {WINDTYPE_KEY: 1})
-
-        with pytest.raises(ValueError):
-            fan.preset_mode = 'not_a_mode'
-
-        with patch(PATCH_SEND_COMMAND) as mock_send_command:
-            fan.fan_speed = 3
-            mock_send_command.assert_called_once_with(fan, {WINDLEVEL_KEY: 3})
-
-        with pytest.raises(ValueError):
-            fan.fan_speed = 10
 
     def test_HTF005S(self):  # pylint: disable=invalid-name
         """Load fan and test sending commands."""
