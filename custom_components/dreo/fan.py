a60f52467e254cc1f878907df63870b7a75fb4be
Jeff Steinbok <jeff@steinbok.net>
2025-05-18 17:11:06 -0700

    Add HUMIDIFIER support in get_entries function for Dreo fan entities

--- custom_components/dreo/fan.py
+++ custom_components/dreo/fan.py
@@ -23,7 +23,8 @@ def get_entries(pydreo_devices : list[PyDreoBaseDevice]) -> list[DreoFanHA]:
         if (pydreo_device.type == DreoDeviceType.TOWER_FAN or
             pydreo_device.type == DreoDeviceType.AIR_CIRCULATOR or
             pydreo_device.type == DreoDeviceType.AIR_PURIFIER or
-            pydreo_device.type == DreoDeviceType.CEILING_FAN):
+            pydreo_device.type == DreoDeviceType.CEILING_FAN or
+            pydreo_device.type == DreoDeviceType.HUMIDIFIER):
             _LOGGER.debug("Fan:get_entries: Found a %s - %s", pydreo_device.type, pydreo_device.name)
             fan_entities_ha.append(DreoFanHA(pydreo_device))
 
