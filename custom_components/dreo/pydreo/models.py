e34c811e1078880771ce02c0665e913ce9308d6b
Jeff Steinbok <jeff@steinbok.net>
2025-10-05 13:27:09 -0700

    Vertical Angle fix for DR-HPF008S (#383)
    * Support Vertical angle range for HPF008S
    
    * Update device names for consistency and add HPF008S device data
    
    * Update device count assertion in debug test mode

--- custom_components/dreo/pydreo/models.py
+++ custom_components/dreo/pydreo/models.py
@@ -24,7 +24,8 @@ from .constant import (
     PRESET_NONE,
     PRESET_ECO,
     PRESET_SLEEP,
-    DreoDeviceType
+    DreoDeviceType,
+    VERTICAL_ANGLE_RANGE
 )
 
 COOKING_MODES = [
@@ -123,7 +124,10 @@ SUPPORTED_DEVICES = {
     "DR-HPF": DreoDeviceDetails(device_type=DreoDeviceType.AIR_CIRCULATOR),
     "DR-HPF008S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
-        device_ranges={SPEED_RANGE: (1, 9)}),
+        device_ranges={
+            SPEED_RANGE: (1, 9), 
+            VERTICAL_ANGLE_RANGE: (-30, 90)
+        }),
 
     "DR-HPF007S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
