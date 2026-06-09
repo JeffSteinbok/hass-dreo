4c606e3035fc00c52b640c88c8ba12b68c724f77
Erick Jeffries <erick.jeffries@gmail.com>
2026-06-09 04:11:42 -0400

    Adding the preset modes and appropriate horizontal and vertical angle ranges for the DR-HPF017S Air Circulator (#728)

--- custom_components/dreo/pydreo/models.py
+++ custom_components/dreo/pydreo/models.py
@@ -294,7 +294,8 @@ SUPPORTED_DEVICES = {
     ),
     "DR-HPF017S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
-        device_ranges={SPEED_RANGE: (1, 12)},
+        preset_modes=[("normal", 1), ("auto", 2), ("sleep", 3), ("natural", 4), ("turbo", 5), ("custom", 6)],
+        device_ranges={SPEED_RANGE: (1, 12), HORIZONTAL_ANGLE_RANGE: (-75, 75), VERTICAL_ANGLE_RANGE: (-30, 90)},
     ),
     "DR-HPF007S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
