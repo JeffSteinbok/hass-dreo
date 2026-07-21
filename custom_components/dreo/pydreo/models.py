b77e3edd19d1eb9b224b47a62a2842901c59e192
Ruckus <130179397+RuckusPNW@users.noreply.github.com>
2026-07-20 20:01:17 -0700

    Update models.py
    Fixed misplaced brace at for DR-HPF007S at line 335

--- custom_components/dreo/pydreo/models.py
+++ custom_components/dreo/pydreo/models.py
@@ -332,7 +332,7 @@ SUPPORTED_DEVICES = {
     "DR-HPF015S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
         preset_modes=[("normal", 1), ("natural", 2), ("sleep", 3), ("auto", 4), ("turbo", 5), ("custom", 6)],
-        device_ranges={SPEED_RANGE: (1, 12), HORIZONTAL_ANGLE_RANGE: (-75, 75)}, VERTICAL_ANGLE_RANGE: (-10, 90)},
+        device_ranges={SPEED_RANGE: (1, 12), HORIZONTAL_ANGLE_RANGE: (-75, 75), VERTICAL_ANGLE_RANGE: (-10, 90)},
     ),
     "DR-HPF017S": DreoDeviceDetails(
         device_type=DreoDeviceType.AIR_CIRCULATOR,
